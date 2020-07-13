54609f5ac7cde955b4c39febf69bac181d4c0400
erdem.io <erdem_arslan_694@hotmail.com>
2020-07-13 17:29:16 +0300

    Client connect version.

---
+++ index.js
@@ -0,0 +1,62 @@
+const socketIO = require('socket.io');
+const io = socketIO(1234);
+var d = new Date();
+// client.js
+const net = require('net');
+var serverSocket=null;
+let localIP;
+
+io.on('connection', (socket) => {
+
+  socket.on('io', (dt) => {
+  //  console.log(dt.command);
+
+
+    if (dt.command == "connect") {
+      console.log(dt.ip);
+      localIP=dt.ip;
+
+      serverSocket = net.createConnection({ port: 5002, host: localIP });
+       serverSocket.on('connect', () => {
+          serverSocket.write("ACK");
+          socket.emit("message","connected");
+          console.log("Connected to " + localIP);
+       });
+       serverSocket.on('error', function(ex) {
+         socket.emit("message","connectionProblem");
+       });
+       serverSocket.on('data', function(data){
+         socket.emit("message",data.toString());
+       });
+
+    }else if (dt.command == "disconnect") {
+      disconnect();
+      socket.emit("message","disconnected");
+    }
+
+    if (dt.command=="toggleOpen") {
+      console.log(dt.line);
+      socket.emit("message", "[Sended] :"+dt.line);
+      serverSocket.write(dt.line);
+
+    }else if(dt.command=="singleCommand"){
+      console.log(dt.line);
+      socket.emit("message", "[Sended] :"+dt.line);
+      serverSocket.write(dt.line);
+    }
+
+  });
+
+  socket.on('disconnect', () => {
+    disconnect();
+    socket.emit("message","disconnected");
+  });
+});
+
+function disconnect(){
+  console.log("Disconnected from " + localIP);
+  if(serverSocket!=null){
+    serverSocket.end();
+  }
+
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const net = require('net');
+
+const connectedSockets = new Set();
+connectedSockets.broadcast = function(data, except) {
+    for (let sock of this)
+        if (sock !== except)
+              sock.write(data.toString());
+}
+const server = net.createServer((c) => {
+  // 'connection' listener.
+  console.log('client connected');
+  connectedSockets.add(c);
+  c.on('end', () => {
+    console.log('client disconnected');
+    connectedSockets.delete(c);
+  });
+
+  c.on('data', data => {
+    console.log("[Received]: "+data.toString());
+    connectedSockets.broadcast(data, c);
+  });
+});
+server.on('error', (err) => {
+  throw err;
+});
+server.listen(5002, () => {
+  console.log('Server mounted and loaded.');
+});
