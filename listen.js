69d1e178fd5b247d909de9281c26dc6bd4112515
erdem.io <erdem_arslan_694@hotmail.com>
2020-07-19 00:29:22 +0300

    Listen side improvement.

--- listen.js
+++ listen.js
@@ -3,9 +3,10 @@ const socketIO = require('socket.io');
 const io = socketIO(1234);
 
 let localIP=5002;
+let socketStatus="close";
 
 let c;
-
+let server;
 const connectedSockets = new Set();
 connectedSockets.broadcast = function(data) {
     for (let sock of this)
@@ -21,21 +22,27 @@ io.on('connection', (socket) => {
     if (dt.command == "connect") {
       socket.emit("message","connected");
       console.log("Listening " + localIP);
-      const server = net.createServer((c) => { connectedSockets.add(c); console.log('client connected');
+      let server = net.createServer((c) => { connectedSockets.add(c); console.log('client connected');
           c.on('end', () => {  console.log('client disconnected');  connectedSockets.delete(c); });
+          c.on('data', data => { socket.emit("message","[Received]: "+data.toString());});
+          c.on('error', (err) => { console.log("something went wrong.");});
       });
 
-      server.on('error', (err) => { console.log("something went wrong."); });
-      server.listen(5002, () => { console.log('Server mounted and loaded.'); });
+      server.on('error', (err) => { console.log("something went wrong."); console.log(err); });
+
+        if(socketStatus=="close"){
+          server.listen(5002, () => { console.log('Server mounted and loaded.'); });
+          socketStatus="open";
+        }
     }else if (dt.command == "disconnect") {
       disconnect();
       socket.emit("message","disconnected");
+      process.exit(22);
     }
 
     if (dt.command=="toggleOpen") {
       console.log(dt.line);
       socket.emit("message", "[Sended] :"+dt.line);
-
       connectedSockets.broadcast(dt.line);
 
     }else if(dt.command=="singleCommand"){
@@ -46,10 +53,11 @@ io.on('connection', (socket) => {
 
   });
 
-  socket.on('disconnect', () => {  disconnect();  socket.emit("message","disconnected"); });
+  socket.on('disconnect', () => { disconnect();  socket.emit("message","disconnected");});
 
 });
 
 function disconnect(){
   console.log("Listening stop.");
+  //socketStatus="close";
 }
